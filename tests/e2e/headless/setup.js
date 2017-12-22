efe960f324340dc07fafba24fa6e2f5f72a1d014
Douglas Duteil <douglasduteil@gmail.com>
2017-12-22 14:48:44 +0100

    chore: run in headless mode by default

--- tests/e2e/headless/setup.js
+++ tests/e2e/headless/setup.js
@@ -5,7 +5,6 @@ const { launch } = require('jest-environment-puppeteer');
 module.exports = async function() {
   return launch({
     args: ['about:blank'],
-    executablePath: process.env.CHROME_BIN,
-    headless: false
+    executablePath: process.env.CHROME_BIN
   });
 };
